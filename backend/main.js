9d4ce83a81f5640dad0ab06f8c69cbbd7a2fb64a
Pravesh Kumar <pravesh4561@gmail.com>
2024-12-28 18:20:18 +0530

    ep commit

--- backend/main.js
+++ backend/main.js
@@ -51,7 +51,7 @@ if (!fs.existsSync(uploadDir)) {
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT || 3306
