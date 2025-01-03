65ca1673cf17324973f2af0e1d09e355d4ca001c
Pravesh Kumar <pravesh4561@gmail.com>
2025-01-03 16:07:39 +0530

    first commit ep commit

--- backend/main.js
+++ backend/main.js
@@ -54,7 +54,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
--- frontend1/src/backend/welcome/Welcome.jsx
+++ frontend1/src/backend/welcome/Welcome.jsx
@@ -255,7 +255,7 @@ function Welcome() {
             <Link
               className="btn d-flex align-items-center"
               type="button"
-              to="http://localhost:5173/"
+              to="/"
               target="_blank"
             >
               <svg
