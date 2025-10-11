cf56e19f2aad576f042a2d26cf41b59fea220106
Pravesh Kumar <pravesh4561@gmail.com>
2025-10-11 11:51:27 +0530

    first commit

--- frontend1/dist/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", function (event) {
-  const data = event.data ? event.data.json() : {};
-  const title = data.title || "New Notification";
-  const options = {
-    body: data.body || "You have a new message!",
-    icon: data.icon || "/Tonic.svg",
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
